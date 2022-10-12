368ca93ec889d86dcb32da568a443ed49ca36664
Kurniawanwan <kurniawanwa42@gmail.com>
2022-10-12 19:36:23 +0700

    Tugas Pertemuan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Kurniawan</li>
+        <li>Nama Belakang :- </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Supadio Gg Ringin Sari 1 Blok E No 43</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sungai Raya</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Sepak Bola</li>
+    </ul>
+</section>
